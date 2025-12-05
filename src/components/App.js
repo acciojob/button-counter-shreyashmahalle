f95818235f88ed39ab2e6909d7f7997c3359641a
Yash <yashmahalle33@gmail.com>
2025-12-05 12:11:54 +0530

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,24 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+    const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(count + 1);
+  };
+
+
   return (
     <div>
         {/* Do not remove the main div */}
+          <p>Button clicked {count} times</p>
+
+      <button onClick={handleClick}>
+        Click Me
+      </button>
     </div>
   )
 }
 
 export default App
+
